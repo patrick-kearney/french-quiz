963b921df08e6874e6847683276585d7fcf4053b
patrick-kearney <patrick@kearney.nz>
2024-11-27 02:44:40 +0000

    Refactored to make the quiz run a class, added new nouns to noun-list.txt

--- noun.py
+++ noun.py
@@ -9,37 +9,103 @@ import random
 # note that ipa data is from: https://github.com/open-dict-data/ipa-dict/ (MIT licence)
 
 # constructing the dictionary of words
-nouns = {}
-with open("noun-list.txt") as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        nouns[row["french"].strip()] = {"gender": row["gender"], 
-            "english": row["english"].strip(),"seen":[], "ipa": '',
-            "count_or_mass": row["count_or_mass"]}
 
-# add the ipa data to the dictionary - there must be a faster way to do this.. (binary search?)
-# should only do it once and then put the saved dictionary into a pickle file?
+class Run:
+    def __init__(self):
+        self.nouns = self.get_nouns()
+        self.words_seen = set()
+        self.run_over = False
+        self.noun_list = list(self.nouns.keys())
 
-word_set = set(nouns.keys())
-with open("fr_FR.txt") as file:
-    reader = csv.DictReader(file, delimiter='\t')
-    for row in reader:
-        if row['word'] in word_set:
-            nouns[row['word']]['ipa'] = row['ipa']
+    def get_nouns(self):
+        ### should make this a default dictionary in case eg some nouns don't have all the features
+        # should also have a skip if noun is already in the dictionary
+        # eventually this will be in it's own file, making the dictionary will
+        # essentially be a one off, this is just to get the program started
+        # also should have a check here to see if word is correctly spelt (ie does it appear
+        # in the IPA list)
+        nouns = {}
+        with open("noun-list.txt") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                nouns[row["french"].strip()] = {"gender": row["gender"], 
+                    "english": row["english"].strip(),"seen":[], "ipa": '',
+                    "countable": row["countable"]}
+        
+        # add the ipa data to the dictionary - there must be a faster way to do this.. (binary search?)
+        # should only do it once and then put the saved dictionary into a pickle file?
+        word_set = set(nouns.keys())
+        with open("fr_FR.txt") as file:
+            reader = csv.DictReader(file, delimiter='\t')
+            for row in reader:
+                if row['word'] in word_set:
+                    nouns[row['word']]['ipa'] = row['ipa']
+        return nouns
 
-print(nouns)
+    def random_noun(self):
+        ### returns a random noun from the list of nouns on this run
+        return random.choice(self.noun_list)
+    
+    def create_question(self):
+    #given a (french) noun and a dictionary of (french) nouns
+    #generate a test question (the english noun with a definite or indefinite article)
+    # and the correct answer (the french noun with the corresponding article)
+        noun = self.random_noun()
+        vowels = ('a', 'e', 'i', 'o', 'u')
+        gender = self.nouns[noun]['gender']
+        english = self.nouns[noun]['english']
+
+        #if mass noun, type is definite, otherwise select at random
+        # mass nouns cannot take an indefinite article
+        if self.nouns[noun]["countable"] == "False": #mass noun
+            type = 'definite'
+            english_article = ''
+        else:
+            type = random.choice(['definite', 'indefinite'])
+            # generate the correct article for english
+            if type == "definite":
+                english_article = "the "
+            elif english.startswith(vowels):
+                english_article = "an "
+            else:
+                english_article = 'a '    
+        english = english_article + english
+
+        # generate correct article for french noun based on the starting letter and article type
+        h_muet = False
+        if noun.startswith('h'):
+                #check if the 'h' is muet (liasion allowed) or aspiré (liasion forbidden)
+                if self.nouns[noun]['ipa'].startswith("/ʼ"):
+                    # le h est aspiré, do nothing, this h behaves like a consonant
+                    pass
+                else:
+                    h_muet = True
+        
+        if type == "definite":
+            if noun.startswith(vowels) or h_muet:
+                french_article = "l'"
+            elif gender == "m":
+                french_article = "le "
+            else:
+                french_article = "la "
+        else:
+            if gender == "m":
+                french_article = "un "
+            else:
+                french_article = "une "
+        french = french_article + noun
+        ipa = self.nouns[noun]['ipa']
+        gender = self.nouns[noun]['gender']
+        return english, french, ipa, gender
+
 
 def main():
-    play_flag = True
-    noun_list = list(nouns.keys())
+    run = Run()
     counter = 0
-    while play_flag:
+    while run.run_over is False:
         now = datetime.datetime.now()
-        noun = random.choice(noun_list)
-        english_test, french_test = create_question(noun,nouns)
-        user_answer = input(f"translate: {english_test}\n").strip()
-        # print(f"the user input is: {user_answer}.")
-        # print(f"the correct answer is: {french_test}.")
+        english,french,ipa,gender = run.create_question()
+        user_answer = input(f"translate: {english}\n").strip()
 
         ## this needs more error checking,
         ## screen for bad user inputs
@@ -47,77 +113,29 @@ def main():
         ## the point is to test gender
         ## not whether they get the difference between definite and indefinite articles correct
         ## the testing code should be tidied into one function
-        if user_answer == french_test:
+        if user_answer.lower() in ('quit', 'q', 'stop'):
+            break
+        if user_answer == french:
             
             print("correct")
         else:
-            print(f"the correct answer is: {french_test}.")
-        print(f"the ipa pronounciation is {nouns[noun]['ipa']}\n")
+            print(f"the correct answer is: {french}.")
+        print(f"the ipa pronounciation is {ipa}\n")
 
-        if french_test.startswith("l'"):
+        if french.startswith("l'"):
                 # need to check the user knows the gender
                 user_gender = input("what is the gender of the noun (m or f)?:\n")
-                if user_gender == nouns[noun]['gender']:
+                if user_gender == gender:
                     print('correct\n')
                 else:
-                    print(f"incorrect, the gender is: {nouns[noun][gender]}\n")
-        
-        
+                    print(f"incorrect, the gender is: {gender}\n")
+              
         counter +=1
-        if counter > 10:
-            play_flag = False
+        if counter > 100:
+            run.run_over = True
     
 
-def create_question(noun,nouns):
-    #given a (french) noun and a dictionary of (french) nouns
-    #generate a test question (the english noun with a definite or indefinite article)
-    # and the correct answer (the french noun with the corresponding article)
-    vowels = ('a', 'e', 'i', 'o', 'u')
-    gender = nouns[noun]['gender']
-    english = nouns[noun]['english']
-    type = random.choice(['definite', 'indefinite'])
 
-    # mass nouns cannot take an indefinite article
-    mass_noun = (nouns[noun]["count_or_mass"] == "mass")
-    if mass_noun:
-        type = "definite"
-        english_article = ''
-    else:
-    # generate the correct article for english
-        if type == "definite":
-            english_article = "the "
-        elif english.startswith(vowels):
-            english_article = "an "
-        else:
-            english_article = 'a '
-    english_test = english_article + english
-
-
-    # generate correct article for french noun based on the starting letter and article type
-    h_muet = False
-    if noun.startswith('h'):
-            #check if the 'h' is muet (liasion allowed) or aspiré (liasion forbidden)
-            if nouns[noun]['ipa'].startswith("/ʼ"):
-                # le h est aspiré, do nothing, this h behaves like a consonant
-                pass
-            else:
-                h_muet = True
-    
-    if type == "definite":
-        if noun.startswith(vowels) or h_muet:
-            french_article = "l'"
-        elif gender == "m":
-            french_article = "le "
-        else:
-            french_article = "la "
-    else:
-        if gender == "m":
-            french_article = "un "
-        else:
-            french_article = "une "
-    french_test = french_article + noun
-
-    return english_test, french_test
 
 if __name__ == "__main__":
     main()
